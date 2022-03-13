361b326226b98bce09f23e6a454b01e83d917ce8
John Wright <jrite@protonmail.com>
2022-03-13 21:32:55 +0000

    fix: remove pnp

--- test/ing.test.ts
+++ test/ing.test.ts
@@ -1,14 +1,11 @@
 import { testkit } from 'graphql-modules'
 import application from '../src/application'
 import authorsModule from '../src/modules/authors'
-import { connectionProvider } from '../src/providers/db'
 
 test('ing', async () => {
-  const app = testkit.mockApplication(await application()).replaceModule(
-    testkit.mockModule(authorsModule, {
-      providers: [await connectionProvider('test')],
-    })
-  )
+  const app = testkit
+    .mockApplication(await application())
+    .replaceModule(testkit.mockModule(authorsModule, {}))
 
   expect(app.schema.getQueryType()).toBeDefined()
 })
